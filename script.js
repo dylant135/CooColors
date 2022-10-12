2ce35ad01d3724ab6a2b19b107ec9f575b9ee052
dylan <dylangthomas1@gmail.com>
2022-10-12 10:26:39 -0600

    wrapped the cell in pop in quotes, but i eventually figured it out

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const container = document.getElementById('container');
 const submit = document.getElementById('submit');
 const inp = document.getElementById('inp');
 
+let arr = [];
+let cell;
+
 submit.addEventListener('click', start);
 function start() {
     let num = inp.value;
+    if(num > 16 || num < 1) {
+        alert('Enter a number 1-16');
+        return;
+    }
     submit.style.display = 'none';
     inp.style.display = 'none';
     container.style.display = 'grid';
@@ -12,8 +19,23 @@ function start() {
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     num = num * num;
     for(let i = 0; i < num; i++) {
-        const cell = document.createElement('div');
+        cell = document.createElement('div');
         cell.classList.add('cell');
+        arr.push(cell);
         container.appendChild(cell);
     }
+    addColor();
+}
+
+function addColor() {
+    arr.sort(() => 0.5 - Math.random());
+    for(let i = 0; i < arr.length; i++) {
+        let color = generateRandomColor();
+        arr[i].style.backgroundColor = color;
+    }
+}
+
+function generateRandomColor() {
+    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
 }
