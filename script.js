f4c07984aa986fc89f5ba33fccd967ff3270c47a
dylan <dylangthomas1@gmail.com>
2022-10-12 09:58:10 -0600

    dynamic grid

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-ECHO is on.
+const container = document.getElementById('container');
+const submit = document.getElementById('submit');
+const inp = document.getElementById('inp');
+
+submit.addEventListener('click', start);
+function start() {
+    let num = inp.value;
+    submit.style.display = 'none';
+    inp.style.display = 'none';
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    num = num * num;
+    for(let i = 0; i < num; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
+}
